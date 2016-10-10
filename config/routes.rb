3fb5374c4ecfd35716da1274e18fbadb7ae88762
cassaram09 <cassaram09@gmail.com>
2016-10-10 12:23:23 -0400

    remove active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
   root to: 'home#index'
 
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", registrations: 'users/registrations' }, skip: [:sessions] 
